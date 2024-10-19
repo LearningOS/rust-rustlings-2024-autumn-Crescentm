58a3c38431a340f11ea40afb800b2a1a72bc599b
Crescentm <guyues@outlook.com>
2024-10-20 00:01:11 +0800

    finish almost except algroithm :cry

--- exercises/algorithm/algorithm3.rs
+++ exercises/algorithm/algorithm3.rs
@@ -1,12 +1,42 @@
 /*
-	sort
-	This problem requires you to implement a sorting algorithm
-	you can use bubble sorting, insertion sorting, heap sorting, etc.
+    sort
+    This problem requires you to implement a sorting algorithm
+    you can use bubble sorting, insertion sorting, heap sorting, etc.
 */
 // I AM NOT DONE
 
-fn sort<T>(array: &mut [T]){
-	//TODO
+fn sort<T>(array: &mut [T])
+where
+    T: Ord,
+{
+    let right = if array.len() == 0 {
+        return;
+    } else {
+        array.len() - 1
+    };
+    let left = 0;
+    if left >= right {
+        return;
+    }
+    let mut low = left;
+    let mut high = right;
+    while low < high {
+        while low < high && array[high] >= array[left] {
+            high -= 1;
+        }
+        while low < high && array[low] <= array[left] {
+            low += 1;
+        }
+        array.swap(low, high);
+    }
+    array.swap(low, left);
+    sort(&mut array[0..low + 1]);
+    sort(&mut array[low + 1..]);
+}
+
+fn main() {
+    let mut vec = vec![37, 73, 57, 75, 91, 19, 46, 64];
+    sort(&mut vec);
 }
 #[cfg(test)]
 mod tests {
@@ -18,13 +48,13 @@ mod tests {
         sort(&mut vec);
         assert_eq!(vec, vec![19, 37, 46, 57, 64, 73, 75, 91]);
     }
-	#[test]
+    #[test]
     fn test_sort_2() {
         let mut vec = vec![1];
         sort(&mut vec);
         assert_eq!(vec, vec![1]);
     }
-	#[test]
+    #[test]
     fn test_sort_3() {
         let mut vec = vec![99, 88, 77, 66, 55, 44, 33, 22, 11];
         sort(&mut vec);
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -16,7 +16,7 @@
 
 // Put your function here!
 fn calculate_price_of_apples(number: i32) -> i32 {
-
+ 
     if number > 40 {
         number
     } else {
